5b6d99d1af4af41563798f70d591a611a128cd4b
hildurk <hildurknuts@gmail.com>
2017-08-25 18:05:59 +0000

    hildur update with model connected

---
+++ sequanceLogoPlot.py
@@ -0,0 +1,86 @@
+## MODIFIED FROM: Author: Saket Choudhar [saketkc\\gmail]
+## License: GPL v3
+## Copyright © 2017 Saket Choudhary<saketkc__AT__gmail>
+
+#%matplotlib inline
+import seaborn
+import matplotlib.pyplot as plt
+plt.style.use('seaborn-ticks')
+from matplotlib import transforms
+import matplotlib.patheffects
+from matplotlib.font_manager import FontProperties
+import matplotlib as mpl
+
+import numpy as np
+
+COLOR_SCHEME = {'G': 'orange', 
+                'A': 'red', 
+                'C': 'blue', 
+                'T': 'darkgreen'}
+
+BASES = list(COLOR_SCHEME.keys())
+
+
+
+class Scale(matplotlib.patheffects.RendererBase):
+    def __init__(self, sx, sy=None):
+        self._sx = sx
+        self._sy = sy
+
+    def draw_path(self, renderer, gc, tpath, affine, rgbFace):
+        affine = affine.identity().scale(self._sx, self._sy)+affine
+        renderer.draw_path(gc, tpath, affine, rgbFace)
+
+def modified_draw_logo(all_scores, DNA_label=['A','C','G','T'], fontfamily='Arial', size=80):
+    if fontfamily == 'xkcd':
+        plt.xkcd()
+    else:
+        mpl.rcParams['font.family'] = fontfamily
+
+    fig, ax = plt.subplots(figsize=(len(all_scores), 2.5))
+
+    font = FontProperties()
+    font.set_size(size)
+    font.set_weight('bold')
+    
+    #font.set_family(fontfamily)
+
+    ax.set_xticks(range(1,len(all_scores)+1))    
+    #ax.set_yticks(range(0,2))
+    ax.set_xticklabels(range(1,len(all_scores)+1), rotation=90)
+    #ax.set_yticklabels(np.arange(0,3,1))  
+    ax.set_yticklabels([])  
+    seaborn.despine(ax=ax, trim=True)
+    
+    trans_offset = transforms.offset_copy(ax.transData, 
+                                          fig=fig, 
+                                          x=1, 
+                                          y=0, 
+                                          units='dots')
+   
+    for index, scores in enumerate(all_scores):
+        yshift = 0
+        for DNAindex, score in enumerate(scores):
+            txt = ax.text(index+1, 
+                          0, 
+                          DNA_label[DNAindex], 
+                          transform=trans_offset,
+                          fontsize=80, 
+                          color=COLOR_SCHEME[DNA_label[DNAindex]],
+                          ha='center',
+                          fontproperties=font
+                         )
+            txt.set_path_effects([Scale(1.0, score*4)])
+            fig.canvas.draw()
+            window_ext = txt.get_window_extent(txt._renderer)
+            yshift = window_ext.height*score*4
+            trans_offset = transforms.offset_copy(txt._transform, 
+                                                  fig=fig,
+                                                  y=yshift,
+                                                  units='points')
+        trans_offset = transforms.offset_copy(ax.transData, 
+                                              fig=fig, 
+                                              x=1, 
+                                              y=0, 
+                                              units='points')    
+    plt.show()
